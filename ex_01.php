5e13b049264a7606f3df365706b4038ed6457680
Hindzch <hind.zaouch@epitech.eu>
2024-11-29 18:37:39 +0100

    Update ex_01.php

--- ex_01.php
+++ ex_01.php
@@ -1,30 +1,14 @@
 <?php
-function rev_epur_str($str) {
-    // Vrf si la chaîne est valide
-    if (!isset($str) || !is_string($str)) {
+function rev_epur_str($chaine) {
+    if (!isset($chaine) || !is_string($chaine)) {
         return false;
     }
 
-
-    $cleaned = preg_replace('/\s+/', ' ', trim($str));
-
-
-    $words = explode(' ', $cleaned);
-
-
-    $reversed = array_reverse($words);
-
-    // Rejoint les mots dans une chaîne avec un espace
-    return implode(' ', $reversed);
-}
-
-// exp
-$input = "   Bonjour    le   monde\t!";
-$result = rev_epur_str($input);
-if ($result !== false) {
-    echo $result; // Affiche : "monde le Bonjour"
-} else {
-    echo "La chaîne est invalide.";
+    $chaine = trim($chaine);
+    $chaine = preg_replace('/\s+/', ' ', $chaine);
+    $mots = explode(' ', $chaine);
+    $mots = array_reverse($mots);
+    return implode(' ', $mots);
 }
 ?>
 
