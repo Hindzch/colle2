6ac0fa94d18630fc1ecec575e1e5c54d9981e0fd
Hindzch <hind.zaouch@epitech.eu>
2024-11-29 18:44:08 +0100

    Update ex_03.php

--- ex_03.php
+++ ex_03.php
@@ -1,2 +1,23 @@
+<?php
+function resum_join_str($chaine1, $chaine2) {
+    if (!isset($chaine1) || !isset($chaine2)) {
+        return false; // Retourne FALSE si une des chaînes est manquante
+    }
+
+    $premierePartie = substr($chaine1, 0, 14);
+    $deuxiemePartie = substr($chaine2, -3);
+
+    // Compléter avec des points si les chaînes sont trop courtes
+    while (strlen($premierePartie) < 14) {
+        $premierePartie .= '.';
+    }
+
+    while (strlen($deuxiemePartie) < 3) {
+        $deuxiemePartie = '.' . $deuxiemePartie;
+    }
+
+    return $premierePartie . $deuxiemePartie;
+}
+?>
 
 
